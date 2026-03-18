a6e3569b27161a2c030a700fd28816b02a6527a0
Bulat <bsharifullin@fix.ru>
2026-03-18 16:48:29 +0300

    создание контроллеров

---
+++ app/controllers/ProcessorController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers;
+
+use app\services\interfaces\LoanProcessorServiceInterface;
+use Yii;
+use yii\base\Module;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+class ProcessorController extends Controller
+{
+    public $enableCsrfValidation = false;
+
+    public function __construct(
+        string $id,
+        Module $module,
+        private readonly LoanProcessorServiceInterface $loanProcessorService,
+        array $config = []
+    ) {
+        parent::__construct($id, $module, $config);
+    }
+
+    /**
+     * @return array{result: bool}
+     * @throws BadRequestHttpException
+     */
+    public function actionIndex(): array
+    {
+        $rawDelay = Yii::$app->request->get('delay', 0);
+
+        if (!ctype_digit((string) $rawDelay)) {
+            throw new BadRequestHttpException('Parameter "delay" must be a non-negative integer.');
+        }
+
+        $this->loanProcessorService->processAll((int) $rawDelay);
+
+        return ['result' => true];
+    }
+}
---
+++ app/controllers/RequestsController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers;
+
+use app\services\interfaces\LoanRequestServiceInterface;
+use Yii;
+use yii\base\Module;
+use yii\web\Controller;
+
+class RequestsController extends Controller
+{
+    public $enableCsrfValidation = false;
+
+    public function __construct(
+        string $id,
+        Module $module,
+        private readonly LoanRequestServiceInterface $loanRequestService,
+        array $config = []
+    ) {
+        parent::__construct($id, $module, $config);
+    }
+
+    /**
+     * @return array{result: bool, id?: int}
+     */
+    public function actionCreate(): array
+    {
+        $data   = Yii::$app->request->bodyParams;
+        $result = $this->loanRequestService->create($data);
+
+        Yii::$app->response->statusCode = $result['result'] ? 201 : 400;
+
+        return $result;
+    }
+}
